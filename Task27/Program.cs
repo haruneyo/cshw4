1a3f7ba748ab0a2290e7e62c2c1d1ad86fb20207
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-03 18:14:05 +0500

    Task 27 done

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+if (num < 0) num = -num;
+for (int i = 1; num >= 1; i++)
+{
+    int digit = num % 10;
+    sum += digit;
+    num /= 10;
+}
+System.Console.WriteLine(sum);
