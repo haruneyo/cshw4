36d9f3f3cb29d304c6d60bca8e4717435b834ba4
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-03 18:28:28 +0500

    Task 29 done

---
+++ Task29/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter the number of elements in the array:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+FillArray(n, array);
+void FillArray(int n, int[]a)
+{
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = new Random().Next(-100, 100);
+    }
+}
+for (int i = 0; i < n; i++) System.Console.WriteLine(array[i]);
